9c1f48ae2c928a372a0bc7bfcf869c055ed4d200
danielcho <mhcho.dev@gmail.com>
2024-03-21 23:25:06 -0600

    jjam

---
+++ src/main/java/mx/com/algoritmo/chap02/CloneArray.java
@@ -0,0 +1,18 @@
+package mx.com.algoritmo.chap02;
+
+public class CloneArray {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5};
+        int[] b = a.clone();    // b는 a의 복제를 참조
+
+        b[3] = 0;               // 한 요소에만 0을 대입
+
+        System.out.print("a =");
+        for(int i=0; i < a.length; i++)
+            System.out.print(" "+a[i]);
+
+        System.out.print("\nb =");
+        for(int i=0; i < b.length; i++)
+            System.out.print(" " + b[i]);
+    }
+}
